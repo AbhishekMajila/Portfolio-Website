7f7534047ed66dd4932eac296908058ab65a3870
Abhishek Majila <abhiithakur0904@gmail.com>
2025-06-26 17:25:50 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: '  Abhsihek Majila Portfolio',
+  description: "Portfolio of Abhishek Majila - Projects, Skills, Contact and More.",
+  
 }
 
 export default function RootLayout({
